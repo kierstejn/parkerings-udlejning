b2c56599a1adc471ae0520154dc3e93f886dd1a3
Sebastian <sebastian@hococo.io>
2026-02-25 16:38:50 +0100

    Changes

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 
-/*Route::get('/test', function (Request $request) {
-    return "test6";
+Route::get('/test', function () {
+    return 'test';
 });
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/login',[UserAuthController::class,'login']);
-Route::post('/logout',[UserAuthController::class,'logout'])
-    ->middleware('auth:sanctum');*/
-
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [UserAuthController::class, 'login']);
